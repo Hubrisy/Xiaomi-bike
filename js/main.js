89d4945adf88a17ba26da2d37f4c819f4b72893d
Hubrisy <79149193+Hubrisy@users.noreply.github.com>
2022-07-25 12:35:34 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('.bike__slider').slick({
+        dots:true,
+        arrows:false,
+      });
+});
+
+$(function(){
+  $('.bike__slider-second').slick({
+      dots:true,
+      arrows:false,
+    });
+});
+
+// $(function(){
+//   $('.choose__bike').slick({
+//       dots:true,
+//       arrows:false,
+//     });
+// });
+
